afea28470ce82b56844893edc37236ed016e8296
omar valdez <omar.valdez.becerril@gmail.com>
2026-03-08 22:20:43 -0600

    secure API calls

--- serverless.ts
+++ serverless.ts
@@ -1,5 +1,12 @@
 import type { AWS } from '@serverless/typescript';
 
+
+const cognitoAuthorizer = {
+    name: 'CognitoAuthorizer',
+    type: 'COGNITO_USER_POOLS',
+    arn: { 'Fn::GetAtt': ['CognitoUserPool', 'Arn'] },
+};
+
 const serverlessConfiguration: AWS = {
     service: 'gestion-construccion',
 
@@ -50,83 +57,83 @@ const serverlessConfiguration: AWS = {
     functions: {
         createLocation: {
             handler: 'src/handlers/locations.createLocation',
-            events: [{ http: { method: 'post', path: 'locations', cors: true } }],
+            events: [{ http: { method: 'post', path: 'locations', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         listLocations: {
             handler: 'src/handlers/locations.listLocations',
-            events: [{ http: { method: 'get', path: 'locations', cors: true } }],
+            events: [{ http: { method: 'get', path: 'locations', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         getLocation: {
             handler: 'src/handlers/locations.getLocation',
-            events: [{ http: { method: 'get', path: 'locations/{id}', cors: true } }],
+            events: [{ http: { method: 'get', path: 'locations/{id}', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         updateLocation: {
             handler: 'src/handlers/locations.updateLocation',
-            events: [{ http: { method: 'put', path: 'locations/{id}', cors: true } }],
+            events: [{ http: { method: 'put', path: 'locations/{id}', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         deleteLocation: {
             handler: 'src/handlers/locations.deleteLocation',
-            events: [{ http: { method: 'delete', path: 'locations/{id}', cors: true } }],
+            events: [{ http: { method: 'delete', path: 'locations/{id}', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         createEmployee: {
             handler: 'src/handlers/employees.createEmployee',
-            events: [{ http: { method: 'post', path: 'employees', cors: true } }],
+            events: [{ http: { method: 'post', path: 'employees', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         listEmployees: {
             handler: 'src/handlers/employees.listEmployees',
-            events: [{ http: { method: 'get', path: 'employees', cors: true } }],
+            events: [{ http: { method: 'get', path: 'employees', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         getEmployee: {
             handler: 'src/handlers/employees.getEmployee',
-            events: [{ http: { method: 'get', path: 'employees/{id}', cors: true } }],
+            events: [{ http: { method: 'get', path: 'employees/{id}', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         updateEmployee: {
             handler: 'src/handlers/employees.updateEmployee',
-            events: [{ http: { method: 'put', path: 'employees/{id}', cors: true } }],
+            events: [{ http: { method: 'put', path: 'employees/{id}', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         deleteEmployee: {
             handler: 'src/handlers/employees.deleteEmployee',
-            events: [{ http: { method: 'delete', path: 'employees/{id}', cors: true } }],
+            events: [{ http: { method: 'delete', path: 'employees/{id}', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         createTool: {
             handler: 'src/handlers/tools.createTool',
-            events: [{ http: { method: 'post', path: 'tools', cors: true } }],
+            events: [{ http: { method: 'post', path: 'tools', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         listTools: {
             handler: 'src/handlers/tools.listTools',
-            events: [{ http: { method: 'get', path: 'tools', cors: true } }],
+            events: [{ http: { method: 'get', path: 'tools', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         getTool: {
             handler: 'src/handlers/tools.getTool',
-            events: [{ http: { method: 'get', path: 'tools/{id}', cors: true } }],
+            events: [{ http: { method: 'get', path: 'tools/{id}', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         updateTool: {
             handler: 'src/handlers/tools.updateTool',
-            events: [{ http: { method: 'put', path: 'tools/{id}', cors: true } }],
+            events: [{ http: { method: 'put', path: 'tools/{id}', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         deleteTool: {
             handler: 'src/handlers/tools.deleteTool',
-            events: [{ http: { method: 'delete', path: 'tools/{id}', cors: true } }],
+            events: [{ http: { method: 'delete', path: 'tools/{id}', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         createProduct: {
             handler: 'src/handlers/products.createProduct',
-            events: [{ http: { method: 'post', path: 'products', cors: true } }],
+            events: [{ http: { method: 'post', path: 'products', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         listProducts: {
             handler: 'src/handlers/products.listProducts',
-            events: [{ http: { method: 'get', path: 'products', cors: true } }],
+            events: [{ http: { method: 'get', path: 'products', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         getProduct: {
             handler: 'src/handlers/products.getProduct',
-            events: [{ http: { method: 'get', path: 'products/{id}', cors: true } }],
+            events: [{ http: { method: 'get', path: 'products/{id}', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         updateProduct: {
             handler: 'src/handlers/products.updateProduct',
-            events: [{ http: { method: 'put', path: 'products/{id}', cors: true } }],
+            events: [{ http: { method: 'put', path: 'products/{id}', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         deleteProduct: {
             handler: 'src/handlers/products.deleteProduct',
-            events: [{ http: { method: 'delete', path: 'products/{id}', cors: true } }],
+            events: [{ http: { method: 'delete', path: 'products/{id}', cors: true, authorizer: cognitoAuthorizer  } }],
         },
 
         registerInventoryMovement: {
@@ -155,71 +162,71 @@ const serverlessConfiguration: AWS = {
         },
         createUser: {
             handler: 'src/handlers/users.createUser',
-            events: [{ http: { method: 'post', path: 'users', cors: true } }],
+            events: [{ http: { method: 'post', path: 'users', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         listUsers: {
             handler: 'src/handlers/users.listUsers',
-            events: [{ http: { method: 'get', path: 'users', cors: true } }],
+            events: [{ http: { method: 'get', path: 'users', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         toggleUserStatus: {
             handler: 'src/handlers/users.toggleUserStatus',
-            events: [{ http: { method: 'patch', path: 'users/{username}/status', cors: true } }],
+            events: [{ http: { method: 'patch', path: 'users/{username}/status', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         registerAttendance: {
             handler: 'src/handlers/attendance.registerAttendance',
-            events: [{ http: { method: 'post', path: 'attendance', cors: true } }],
+            events: [{ http: { method: 'post', path: 'attendance', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         listAttendance: {
             handler: 'src/handlers/attendance.listAttendance',
-            events: [{ http: { method: 'get', path: 'attendance', cors: true } }],
+            events: [{ http: { method: 'get', path: 'attendance', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         reportAttendance: {
             handler: 'src/handlers/attendance.reportAttendance',
-            events: [{ http: { method: 'get', path: 'attendance/report', cors: true } }],
+            events: [{ http: { method: 'get', path: 'attendance/report', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         registerToolMovement: {
             handler: 'src/handlers/toolRecord.registerToolMovement',
-            events: [{ http: { method: 'post', path: 'tool-records', cors: true } }],
+            events: [{ http: { method: 'post', path: 'tool-records', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         listToolRecords: {
             handler: 'src/handlers/toolRecord.listToolRecords',
-            events: [{ http: { method: 'get', path: 'tool-records', cors: true } }],
+            events: [{ http: { method: 'get', path: 'tool-records', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         createVehicle: {
             handler: 'src/handlers/vehicles.createVehicle',
-            events: [{ http: { method: 'post', path: 'vehicles', cors: true } }],
+            events: [{ http: { method: 'post', path: 'vehicles', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         listVehicles: {
             handler: 'src/handlers/vehicles.listVehicles',
-            events: [{ http: { method: 'get', path: 'vehicles', cors: true } }],
+            events: [{ http: { method: 'get', path: 'vehicles', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         getVehicle: {
             handler: 'src/handlers/vehicles.getVehicle',
-            events: [{ http: { method: 'get', path: 'vehicles/{id}', cors: true } }],
+            events: [{ http: { method: 'get', path: 'vehicles/{id}', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         updateVehicle: {
             handler: 'src/handlers/vehicles.updateVehicle',
-            events: [{ http: { method: 'put', path: 'vehicles/{id}', cors: true } }],
+            events: [{ http: { method: 'put', path: 'vehicles/{id}', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         deleteVehicle: {
             handler: 'src/handlers/vehicles.deleteVehicle',
-            events: [{ http: { method: 'delete', path: 'vehicles/{id}', cors: true } }],
+            events: [{ http: { method: 'delete', path: 'vehicles/{id}', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         registerVehicleMovement: {
             handler: 'src/handlers/vehicleRecord.registerMovement',
-            events: [{ http: { method: 'post', path: 'vehicle-records', cors: true } }],
+            events: [{ http: { method: 'post', path: 'vehicle-records', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         getVehicleHistory: {
             handler: 'src/handlers/vehicleRecord.getVehicleHistory',
-            events: [{ http: { method: 'get', path: 'vehicle-records/{id}', cors: true } }],
+            events: [{ http: { method: 'get', path: 'vehicle-records/{id}', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         getAllVehicleRecords: {
             handler: 'src/handlers/vehicleRecord.getAllVehicleRecords',
-            events: [{ http: { method: 'get', path: 'vehicle-records', cors: true } }],
+            events: [{ http: { method: 'get', path: 'vehicle-records', cors: true, authorizer: cognitoAuthorizer  } }],
         },
         generateUploadUrl: {
             handler: 'src/handlers/upload.generateUploadUrl',
-            events: [{ http: { method: 'get', path: 'upload-url', cors: true } }],
+            events: [{ http: { method: 'get', path: 'upload-url', cors: true, authorizer: cognitoAuthorizer  } }],
         },
     },
     package: { individually: true },
